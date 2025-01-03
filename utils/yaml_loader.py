36455317a6cbc88abe83ccbbab5b60562a97a41d
Chester Lee <dylizc@chint.com>
2025-01-03 12:46:07 +0800

    Enhance .gitignore to exclude log and test-related filesp; Update yaml_loader to support both .yaml and .yml file extensions; Add yaml_loader test

--- utils/yaml_loader.py
+++ utils/yaml_loader.py
@@ -5,8 +5,11 @@ from typing import Dict
 
 def load_yaml_files(directory: Path) -> Dict[str, Dict[str, str]]:
     translations = {}
-    for yaml_file in glob.glob(str(directory / "*.yaml")):
+    
+    yaml_files = glob.glob(str(directory / "*.yml")) + glob.glob(str(directory / "*.yaml"))
+    for yaml_file in yaml_files:
         with open(yaml_file, "r", encoding="utf-8") as f:
+            # YAML file name should be like "en-GB.yml". Then language code will be "en-GB"
             lang_code = Path(yaml_file).stem
             translations[lang_code] = yaml.safe_load(f)
     return translations
